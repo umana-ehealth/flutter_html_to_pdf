4c6152f0fb579c01c5285725b17ede68508567c1
Marino <36266749+sh3rlock97@users.noreply.github.com>
2021-07-30 11:18:54 +0200

    landscape android

--- android/src/main/kotlin/com/afur/flutterhtmltopdf/HtmlToPdfConverter.kt
+++ android/src/main/kotlin/com/afur/flutterhtmltopdf/HtmlToPdfConverter.kt
@@ -37,7 +37,7 @@ class HtmlToPdfConverter {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 
             val attributes = PrintAttributes.Builder()
-                    .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
+                    .setMediaSize(PrintAttributes.MediaSize.ISO_A4.asLandscape())
                     .setResolution(PrintAttributes.Resolution("pdf", "pdf", 600, 600))
                     .setMinMargins(PrintAttributes.Margins.NO_MARGINS).build()
 
